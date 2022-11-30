9b74aa576f1f08e171aea33b89036e9699e5a20f
jenya.jon <pavlinok.e@gmail.com>
2022-11-30 15:30:59 +0300

    Add nothig1

--- 1_lesson/1.4/Program.cs
+++ 1_lesson/1.4/Program.cs
@@ -1,6 +1,21 @@
-﻿// Напишите программу вычисления модуля числа.
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
 
-Console.Write("Введите число: ");
-var s = Console.ReadLine();
-int N = s == null ? 0 : int.Parse(s);
-Console.WriteLine($"{Math.Abs(N)}");
+Console.WriteLine("Введите число 1");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("Число 1 больше");
+}
+else if(num2 > num1)
+{
+    Console.WriteLine("Число 2 больше");
+}
+else
+{
+    Console.WriteLine("Число 1 = 2 больше");
+}
