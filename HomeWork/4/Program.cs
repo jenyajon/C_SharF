28a0dc7bcfec51c78fa17522fc1cbecb30e0bfd7
jenya.jon <pavlinok.e@gmail.com>
2022-11-30 12:12:25 +0300

    Add third task

---
+++ HomeWork/4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int N = int.Parse(num);
+count = 0;
+
+
+if(N == 2)
+{
+    Console.WriteLine("2 и все");
+}
+
+while(count == N)
+{
+    Console.WriteLine(count);
+    count+=2;
+}
