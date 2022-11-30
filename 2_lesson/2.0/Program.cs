70c08b3320a7c34c5a32b55bf19e3359bb58e2fd
jenya.jon <pavlinok.e@gmail.com>
2022-11-30 20:59:31 +0300

    Add 2.0

---
+++ 2_lesson/2.0/Program.cs
@@ -0,0 +1,14 @@
+﻿// 0. Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int MaxNumber(int num)
+{
+Console.WriteLine(num);
+
+if (num % 10 > num / 10)
+return num % 10;
+else
+return num / 10;
+}
+
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
