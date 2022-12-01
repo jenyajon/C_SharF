c4032dc70787476ed7056a189695b8647e7b5c4e
jenya.jon <pavlinok.e@gmail.com>
2022-12-01 17:39:31 +0300

    Add 2.3

---
+++ 2_lesson/2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно 7 и 23.
+
+string Func(int num)
+{
+if(num % 7 == 0)
+return $"Число {num} кратное 7";
+else
+return $"Число {num} не кратное и имеет остаток от 7: {num % 7}";
+if(num % 23 == 0)
+return $"Число {num} кратное 23";
+else
+return $"Число {num} не кратное и имеет остаток от 23 {num % 23}";
+}
+Console.Write("Введите число для проверки:");
+int nuu = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(Func(nuu));
