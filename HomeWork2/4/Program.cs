64ac928555abbd701568fa2baabf8dee09290513
jenya.jon <pavlinok.e@gmail.com>
2022-12-01 19:32:50 +0300

    Add HW4

---
+++ HomeWork2/4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Numb(int num)
+{
+    string text = "no";
+    if (num == 7 || num == 6) text = "yes";
+    Console.WriteLine($"{num} --> {text}");
+}
+Numb(int.Parse(Console.ReadLine()));
