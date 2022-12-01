39410cbf91de264d31898809812b282f2f9abb22
jenya.jon <pavlinok.e@gmail.com>
2022-12-01 19:27:18 +0300

    Add HW3

---
+++ HomeWork2/3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру
+//           заданного числа или сообщает, что третьей цифры нет.
+
+void Func(int num)
+{
+    Console.Write($"{num} --> ");
+    if (num < 99)
+    {
+        Console.Write("Число не 3е число");
+        return;
+    }
+    while (num > 999) num /=10;
+    Console.WriteLine(num % 10);
+}
+
+Func(int.Parse(Console.ReadLine()));
