cbe4eb973eb634133123316dea4dea3631ee6658
jenya.jon <pavlinok.e@gmail.com>
2022-12-09 11:57:43 +0300

    Add HW 3

---
+++ HomeWork3/3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Auf(int N)
+{
+int i = 0;
+double sum;
+while (i < N )
+{
+i += 1;
+if (i == (N))
+{
+sum = Math.Pow(i,3);
+Console.Write($"{sum}");
+}
+else
+{
+
+sum = Math.Pow(i,3);
+Console.Write($"{sum},");
+}
+}
+
+}
+Console.WriteLine("введите число");
+int chislo = int.Parse(Console.ReadLine());
+Auf(chislo);
