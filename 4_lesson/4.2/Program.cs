221cb1af37fb848db02765faf39d1bf096d03404
jenya.jon <pavlinok.e@gmail.com>
2022-12-14 12:10:48 +0300

    Add task 4.2

---
+++ 4_lesson/4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+
+int Func(int N)
+{
+    int sum = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+Console.WriteLine(Func(int.Parse(Console.ReadLine())));
