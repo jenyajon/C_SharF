d1b3774e52926f5770d7e1dabeaa4ff2c38d8bb9
jenya.jon <pavlinok.e@gmail.com>
2022-12-14 10:07:34 +0300

    Add task 4.0

---
+++ 4_lesson/4.0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+
+void Func(int N)
+{
+    int i = 0;
+    int sum = 0;
+    while (i <= N)
+    {
+        sum = sum + i;
+        i += 1;
+    }
+    Console.Write($"{sum} ");
+}
+
+Console.Write("введите число: ");
+int chislo = int.Parse(Console.ReadLine());
+Func(chislo);
