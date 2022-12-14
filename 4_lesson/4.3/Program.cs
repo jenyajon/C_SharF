2c7c4f4bd9115f4ba8a393a71733dfe7ba4d6ec1
jenya.jon <pavlinok.e@gmail.com>
2022-12-14 16:28:34 +0300

    Add less4 task 4.3

---
+++ 4_lesson/4.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// 3. Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+void Func(int N)
+{
+    int[] massiv = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        massiv[i] = new Random().Next(2);
+        Console.Write($"{massiv[i]}, ");
+    }
+}
+
+Console.WriteLine("Введите число:");
+Func(int.Parse(Console.ReadLine()));
