6336729bb242e9933c5a4e09a9e8b95fc8a53b7f
jenya.jon <pavlinok.e@gmail.com>
2022-12-20 10:31:22 +0300

    Add task4.2

--- HomeWork4/4.2/Program.cs
+++ HomeWork4/4.2/Program.cs
@@ -1,1 +1,16 @@
-﻿// 
+﻿// Задача 2: Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+
+int Func(int num)
+{
+    int sum = 0;
+
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /=10;
+    }
+    return sum;
+}
+
+Console.WriteLine(Func(int.Parse(Console.ReadLine()))); 
