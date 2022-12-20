ef254ba2053514126e4b3102f5d6a8eabaa7f274
jenya.jon <pavlinok.e@gmail.com>
2022-12-20 11:54:45 +0300

    Add task2

---
+++ 5_lesson/5.0/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+
+void Funcc(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Massiv(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void SumPosNeg(int[] arr)
+{
+    int pos, neg;
+    pos = neg = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >= 0)
+            pos += arr[i];
+        else
+            neg += arr[i];
+    }
+    Console.WriteLine($"Положительные: {pos}, negative: {neg}");
+
+}
+
+int[] arr_1 = Massiv(int.Parse(Console.ReadLine()),
+                     int.Parse(Console.ReadLine()),
+                     int.Parse(Console.ReadLine()));
+Funcc(arr_1);
+SumPosNeg(arr_1);
