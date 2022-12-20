c4ac051b9087d500feb6171fb436092a63e82ed5
jenya.jon <pavlinok.e@gmail.com>
2022-12-20 09:25:41 +0300

    Add task4.1

--- HomeWork4/4.1/Program.cs
+++ HomeWork4/4.1/Program.cs
@@ -1,1 +1,24 @@
-﻿// 
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+// число A в натуральную степень B. Без модуля Math, используем цикл for.
+
+double Func(int a, int b)
+{
+    double n_pow = 1;
+    int b_abs = Math.Abs(b);
+
+    for (int i = 1; i<=b_abs; i++)
+    {
+        if (b > 0);
+            n_pow *=a;
+        else
+            n_pow /=a;
+    }
+    return n_pow;
+}
+
+Console.WriteLine("Введите число: ");
+int num_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int num_2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Func(num_1, num_2));
