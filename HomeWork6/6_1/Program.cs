4d4df24b23c89bd76bce55ec3844bd671b7d46d0
jenya.jon <pavlinok.e@gmail.com>
2022-12-23 09:46:25 +0300

    Add HW1

---
+++ HomeWork6/6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Пользователь вводит с клавиатуры M чисел. 
+//    Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+
+int Check(int num)
+{
+    int count = 0;
+    
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"Число {i}: ");
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Сколько чисел хотели бы ввести?: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество чисел больше 0: {Check(a)}");
