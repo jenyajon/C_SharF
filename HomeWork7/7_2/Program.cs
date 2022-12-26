fdf34989160f3a50eb50746a72d545510f9e95b8
jenya.jon <pavlinok.e@gmail.com>
2022-12-26 16:44:42 +0300

    Add HW7 2

--- HomeWork7/7_2/Program.cs
+++ HomeWork7/7_2/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая на вход принимает позиции элемента
+//    в двумерном массиве, и возвращает значение этого элемента 
+//    или же указание, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+string FindElement(int[,] arr, int f, int s)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    if (f > row || f <= 0 || s > column || s <= 0)
+        return $"{f} {s} -> not in the array";
+    return $"arr[{f}, {s}] = {arr[f - 1, s - 1]} -> is in the array";
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+
+Console.Write("Enter the line position: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("Enter the column position: ");
+int second = int.Parse(Console.ReadLine());
+
+Console.WriteLine(FindElement(arr_1, first, second));
