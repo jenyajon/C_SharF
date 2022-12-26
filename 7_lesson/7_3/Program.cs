75544372bf3127092f5bc429a1dde05376317cd8
jenya.jon <pavlinok.e@gmail.com>
2022-12-26 15:48:26 +0300

    Add 7_3 task

---
+++ 7_lesson/7_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// 4. Задайте двумерный массив. Введите элемент, и найдите первое его вхождение,
+// выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+
+string FindElem(int[,] arr, int findNum)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+for (int j = 0; j < arr.GetLength(1); j++)
+if (arr[i, j] == findNum)
+return $"Искомый элемент {findNum} находится на позиции [{i + 1}, {j + 1}]";
+return $"Искомый элемент {findNum} не найден";
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(FindElem(arr_1, int.Parse(Console.ReadLine())));
