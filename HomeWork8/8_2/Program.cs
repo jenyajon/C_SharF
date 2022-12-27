2e9c8a5da8c1e333e4979107cad0473cf28de85a
jenya.jon <pavlinok.e@gmail.com>
2022-12-27 15:20:04 +0300

    Add task hw 8_2

--- HomeWork8/8_2/Program.cs
+++ HomeWork8/8_2/Program.cs
@@ -1,1 +1,62 @@
-﻿// Se
+﻿// 2. Задайте прямоугольный двумерный массив. Напишите программу,
+//    которая будет находить строку с наименьшей суммой элементов.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],3} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void RowSmallAmount(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int amount = 1000;
+    int num;
+    int smallest = 0;
+
+    for (int i = 0; i < row_size; i++)
+    {   
+        num = 0;
+        for (int j = 0; j < column_size; j++)
+            num += arr[i, j];
+            Console.Write($"{num, 4} ");            
+
+        if (amount > num)
+        {
+            amount = num;
+            smallest = i;
+        }
+    }
+    Console.WriteLine();
+    Console.Write($"The row with the lowest amount per position - {smallest + 1}");
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 0, 10);
+Print(arr_1);
+
+RowSmallAmount(arr_1);
